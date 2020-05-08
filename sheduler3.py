7f85e9a4755ecb273ab0981b5f3c7bafc331573f
openmind <pirogov27@yahoo.com>
2020-05-08 13:11:19 +0300

    some parts are working

--- sheduler3.py
+++ sheduler3.py
@@ -10,14 +10,6 @@ records1 = []
 records2 = []
 records3 = []
 
-
-# events
-write_file1 = threading.Event()
-write_file2 = threading.Event()
-write_file3 = threading.Event()
-read_file = threading.Event()
-
-
 # lock-objects
 lock_file1 = threading.Semaphore()
 lock_file2 = threading.Semaphore()
@@ -37,76 +29,47 @@ def parser():
 
 def write1():
     while True:
-        write_file1.wait()
         print("w1")
-
         lock_file1.acquire()
         if records1 != []:
             with open("file1.json", "w") as file1:
                 json.dump(records1.pop(0), file1, indent=4, sort_keys=True)
         lock_file1.release()
-
-        write_file1.clear()
         
 
 def write2():
     while True:
-        write_file2.wait()
         print("w2")
-
         lock_file2.acquire()
         if records2 != []:
             with open("file2.json", "a") as file2:
                 json.dump(records2.pop(0), file2, indent=4, sort_keys=True)
         lock_file2.release()
 
-        write_file2.clear()
-        
                         
 def write3():
     while True:
-        write_file3.wait()
         print("w3")
-
         lock_file3.acquire()
         if records3 != []:
             with open("file3.json", "a") as file3:
                 json.dump(records3.pop(0), file3, indent=4, sort_keys=True)
         lock_file3.release()
-
-        write_file3.clear()
         
 
 def read():
-    time.sleep(1)
-    write_file1.clear()
-    write_file2.clear()
-    write_file3.clear()
-
-
-    write_file1.set()
-    write_file2.set()
-    write_file3.set()
-
-    time.sleep(1)
-
     while True:
         # 1
         lock_file1.acquire()
-        write_file2.set()
-        write_file3.set()
         print("reading from file 1")
         with open("file1.json", "r") as file:
             data = file.read()
             print(data)
         lock_file1.release()
-        # time.sleep(1)
-
+        
 
         # 2
         lock_file2.acquire()
-        write_file1.set()
-        write_file3.set()
         print("reading from file 2")
         with open("file2.json", "r") as file:
             data = file.read()
@@ -117,8 +80,6 @@ def read():
 
         # 3
         lock_file3.acquire()
-        write_file1.set()
-        write_file2.set()
         print("reading from file 3")
         with open("file3.json", "r") as file:
             data = file.read()
@@ -127,11 +88,7 @@ def read():
         # time.sleep(1)
 
         # 4
-        write_file1.set()
-        write_file2.set()
-        write_file3.set()
         print("not reading")
-        # time.sleep(1)
         
 
 def main():
@@ -140,11 +97,6 @@ def main():
     w3 = threading.Thread(target=write3, args=(), name="w3")
     r = threading.Thread(target=read, args=(), name="r")
     p = threading.Thread(target=parser, args=(), name="p")
-
-    write_file1.clear()
-    write_file2.clear()
-    write_file3.clear()
-    read_file.clear()
 
     w1.start()
     print("start w1")
